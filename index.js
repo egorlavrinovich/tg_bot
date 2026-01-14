0517b57777adfb16631aed51de6a829a15a5d15c
Egorlaptop <egorlavrinovich@gmail.com>
2026-01-14 19:02:14 +0300

    Попытка №6

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import bot from "./src/bot/bot.js";
-import webhook from "./src/routes/webhook.js";
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import bot from "./src/bot/bot.js";
+// import webhook from "./src/routes/webhook.js";
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-(async function run() {
-  try {
-    await mongoose.connect(process.env.DB_TOKEN);
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-})().catch((e) => console.log(e));
+// (async function run() {
+//   try {
+//     await mongoose.connect(process.env.DB_TOKEN);
+//     const PORT = process.env.PORT || 3000;
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// })().catch((e) => console.log(e));
