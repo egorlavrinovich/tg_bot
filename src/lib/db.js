0e7045cbda1de645351aabb7cdfb61816a11e40d
Egorlaptop <egorlavrinovich@gmail.com>
2026-01-14 19:43:12 +0300

    Попытка №11

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,26 +1,20 @@
+import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bot from "./src/bot/bot.js";
+import webhook from "./src/routes/webhook.js";
 
-let cached = global.mongoose;
+const app = express();
+app.use(express.json());
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-export default async function dbConnect() {
+export default async function run() {
   try {
-    if (cached.conn) return cached.conn;
-
-    if (!cached.promise) {
-      cached.promise = mongoose.connect(process.env.DB_TOKEN, {
-        bufferCommands: false,
-        serverSelectionTimeoutMS: 5000,
-      });
-    }
-
-    cached.conn = await cached.promise;
-    return cached.conn;
+    await mongoose.connect(process.env.DB_TOKEN);
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   } catch (e) {
-    console.error("Mongo connection failed");
-    throw e;
+    console.log(e);
   }
 }
