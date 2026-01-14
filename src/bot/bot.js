28da750c23b8055e1c3cdd95f8847010361cd7c6
Egorlaptop <egorlavrinovich@gmail.com>
2026-01-14 18:55:37 +0300

    Попытка №5

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,22 +1,22 @@
-import TelegramBot from "node-telegram-bot-api";
-import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
-import { handleStart } from "../handlers/start.js";
-import { handleRole } from "../handlers/role.js";
-import { handleClientMessage } from "../handlers/customer/clientMessage.js";
-import { handleOrder } from "../handlers/specialist/handleOrder.js";
-import { closeOrder } from "../handlers/customer/closeOrder.js";
-import { performOrder } from "../handlers/customer/performOrder.js";
-import { reviewCandidat } from "../handlers/customer/reviewCandidat.js";
-import { handleSpecialistCategory } from "../handlers/specialist/handleSpecialistCategory.js";
-import { handleSpecialistConfirm } from "../handlers/specialist/handleSpecialistConfirm.js";
-import { handleResendInvites } from "../handlers/specialist/handleResendInvites.js";
-import { handleClientCategorySelect } from "../handlers/handleClientCategorySelect.js";
+// import TelegramBot from "node-telegram-bot-api";
+// import dotenv from "dotenv";
+// dotenv.config({ path: "./.env" });
+// import { handleStart } from "../handlers/start.js";
+// import { handleRole } from "../handlers/role.js";
+// import { handleClientMessage } from "../handlers/customer/clientMessage.js";
+// import { handleOrder } from "../handlers/specialist/handleOrder.js";
+// import { closeOrder } from "../handlers/customer/closeOrder.js";
+// import { performOrder } from "../handlers/customer/performOrder.js";
+// import { reviewCandidat } from "../handlers/customer/reviewCandidat.js";
+// import { handleSpecialistCategory } from "../handlers/specialist/handleSpecialistCategory.js";
+// import { handleSpecialistConfirm } from "../handlers/specialist/handleSpecialistConfirm.js";
+// import { handleResendInvites } from "../handlers/specialist/handleResendInvites.js";
+// import { handleClientCategorySelect } from "../handlers/handleClientCategorySelect.js";
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new TelegramBot(token);
+// const token = process.env.TELEGRAM_BOT_TOKEN;
+// const bot = new TelegramBot(token);
 
-bot.onText(/\/start/, (msg) => handleStart(bot, msg));
+// bot.onText(/\/start/, (msg) => handleStart(bot, msg));
 
 // bot.on("callback_query", async (query) => {
 //   if (query.data.startsWith("role_")) return handleRole(bot, query);
@@ -39,4 +39,4 @@ bot.onText(/\/start/, (msg) => handleStart(bot, msg));
 //   console.log("CHANNEL ID:", msg.chat.id);
 // }); // Нужен для уточнения ид канала
 
-export default bot;
+// export default bot;
