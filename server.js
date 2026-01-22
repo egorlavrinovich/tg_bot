8f239713b3694142ffca56c5f016a14dd5108a6c
Egor Lavr <egorlavrinovich@gmail.com>
2026-01-22 18:39:11 +0300

    Корректировка конфига Sentry

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ if (process.env.SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     environment: process.env.NODE_ENV || "development",
+    integrations: [
+        // send console.log, console.warn, and console.error calls as logs to Sentry
+        Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+      ],
+      // Enable logs to be sent to Sentry
+      enableLogs: true,
   });
 }
 
